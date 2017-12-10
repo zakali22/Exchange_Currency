8337de57fc28b05b058593fa5a8184b457a962a2
Zakeria Ali <zak.fined@gmail.com>
2017-12-10 17:11:50 +0000

    Make the API call

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const http = require('http');
+const https = require('https');
+const argument = process.argv;
+const currency1 = argument[2],
+	  currency2 = argument[3],
+	  baseCurrency = argument[4];
+let result = 0;
 
-const exchange = (curr1, curr2) => {
-	http.get('http://apilayer.net/api/convert', (response) => {
+const exchangeDisplay = (curr1, curr2) => {
+	console.log("1 " + curr1 + " to " + curr2 + " is " + result);
+};
+
+
+const exchange = (curr1, curr2, base) => {
+	https.get(`https://api.fixer.io/latest?base=${base}&symbols=${curr1, curr2}`, (response) => {
 		let fullBody= '';
 		response.on('data', (data) => {
 			fullBody += data;
 		}).on('end', (data) => {
-			console.log(fullBody);
+			let parsedData = JSON.parse(fullBody);
+			result += parsedData["rates"]["GBP"];
+			exchangeDisplay(curr1, curr2);
 		});
 	});
 }
+
+exchange(currency1, currency2, baseCurrency);
